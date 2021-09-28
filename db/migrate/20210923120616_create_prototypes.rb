476edd7a096ad23f9cd809eb6c2e3438691a9f2b
Masahiro6280 <purple08260826@gmail.com>
2021-09-28 12:48:51 +0900

    マイグレーションファイルの修正

--- db/migrate/20210923120616_create_prototypes.rb
+++ db/migrate/20210923120616_create_prototypes.rb
@@ -5,7 +5,7 @@ class CreatePrototypes < ActiveRecord::Migration[6.0]
       t.string :title,                 null: false
       t.text :catch_copy,              null: false
       t.text :concept,                 null: false 
-      t.references :user, foreign_key: true             null: false
+      t.references :user, foreign_key: true,null: false
       # rails db:migrateを実行した後に、修正した
     end
   end
